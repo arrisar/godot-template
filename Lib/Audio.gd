fdb8b4dc9893593e81fdc66a19bf39eb6003a0f6
arrisar <james@arrisar.com.au>
2022-01-25 23:15:58 +1100

    feat: Audio is responsive to Config changes

--- Lib/Audio.gd
+++ Lib/Audio.gd
@@ -1,18 +1,35 @@
 extends Node
 
 
+var BUSES = {}
+
+
 func _ready() -> void:
 	print('[Audio] Starting')
 	_set_volume_from_config()
+	Config.connect('updated', self, '_on_config_updated')
 
 
-func _set_volume_from_config(bus: String = 'Master') -> void:
-	var energy: float = Config.get('system/audio/%s_volume' % bus.to_lower())
-	set_volume(energy, bus)
+func _get_bus_config_path(name: String = 'Master') -> String:
+	return 'system/audio/%s_volume' % name.to_lower()
 
 
-func _get_index(bus: String) -> int:
-	return AudioServer.get_bus_index(bus)
+func _get_buses() -> Array:
+	var buses = []
+	
+	for i in AudioServer.bus_count:
+		buses.append(AudioServer.get_bus_name(i))
+	
+	return buses
+
+
+func _get_index(name: String) -> int:
+	return AudioServer.get_bus_index(name)
+
+
+func _set_volume_from_config(name: String = 'Master') -> void:
+	var energy: float = Config.get(_get_bus_config_path(name), 1.0)
+	set_volume(energy, name)
 
 
 func add_bus(name: String):
@@ -40,3 +57,10 @@ func get_volume(name: String = 'Master') -> float:
 func set_volume(energy: float, name: String = 'Master') -> void:
 	print('[Audio] Setting volume for bus %s to %s' % [name, energy])
 	AudioServer.set_bus_volume_db(_get_index(name), linear2db(energy))
+
+
+func _on_config_updated(path) -> void:
+	for name in _get_buses():
+		var bus_config_path: String = _get_bus_config_path(name)
+		if path == bus_config_path:
+			set_volume(Config.get(bus_config_path, 1.0), name)
--- Main/Main.gd
+++ Main/Main.gd
@@ -4,6 +4,3 @@ extends Node
 func _ready():
 	Server.create()
 	Client.create()
-	
-	Audio.add_bus('Foo')
-	print(Audio.get_volume('Foo'))
